0b95ac4f72e54aaf29ffd05af19733db86be587b
dmsps <34230555+dmsps@users.noreply.github.com>
2019-04-23 18:29:58 +0300

    Updates

---
+++ precache-manifest.92fe1c947626f3783bd7269a44a3a64c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0155c9389d861e57dce6d0fd19fe41ec",
+    "url": "/sib/index.html"
+  },
+  {
+    "revision": "29de764d80e7bcee1b9c",
+    "url": "/sib/static/css/2.fee8f29b.chunk.css"
+  },
+  {
+    "revision": "29de764d80e7bcee1b9c",
+    "url": "/sib/static/js/2.9aa23452.chunk.js"
+  },
+  {
+    "revision": "90a06defe5a0e5ad33a9",
+    "url": "/sib/static/js/main.fd9d771e.chunk.js"
+  },
+  {
+    "revision": "506c3481e863881b473a",
+    "url": "/sib/static/js/runtime~main.e9dc1d69.js"
+  }
+]);
--- src/app/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import Grid from "../grid/Grid";
-
-const App = () => <Grid />;
-
-export default App;
